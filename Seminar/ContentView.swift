897f4ce86f4c88a39cdd8b7983c262e415261861
trinhxuanminh2000 <trinhxuanminh2000@gmail.com>
2024-06-11 14:10:53 +0700

    Initial Commit

---
+++ Seminar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Seminar
+//
+//  Created by Trịnh Xuân Minh on 11/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Seminar/SeminarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeminarApp.swift
+//  Seminar
+//
+//  Created by Trịnh Xuân Minh on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SeminarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
